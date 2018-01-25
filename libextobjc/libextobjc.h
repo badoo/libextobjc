e8dfa703864657542532822232ee5b13810a4b38
Damir Davletov <damir.davletov@corp.badoo.com>
2018-01-25 13:49:20 +0000

    Added iOS Framework target for carthage support

---
+++ libextobjc/libextobjc.h
@@ -0,0 +1,18 @@
+//
+//  libextobjc.h
+//  libextobjc
+//
+//  Created by Damir Davletov on 25/01/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libextobjc.
+FOUNDATION_EXPORT double libextobjcVersionNumber;
+
+//! Project version string for libextobjc.
+FOUNDATION_EXPORT const unsigned char libextobjcVersionString[];
+
+#import <libextobjc/extobjc.h>
+
+
